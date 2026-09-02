447c7d4f06efcb7ea75884410a276c51322ee51b
rashique14 <r_itquan@live.concordia.ca>
2026-09-01 20:52:14 -0400

    Modify benders.jl to expand node range and constraints
    Updated the model to include additional nodes and adjusted constraints accordingly.

--- benders.jl
+++ benders.jl
@@ -13,8 +13,8 @@ new_model() = Model(() -> Gurobi.Optimizer(GRB_ENV))
 # ======================================================================
 Random.seed!(90)
 
-const NODES  = 1:10
-const NODES0 = 0:10
+const NODES  = 1:12
+const NODES0 = 0:12
 const T_END  = 30.0
 const U_MAX  = 100.0
 
