15f5604f8246e90c0d96ec9710dba240f45010ab
rashique14 <r_itquan@live.concordia.ca>
2026-09-08 14:47:14 -0400

    Refactor model to use fixed route and optimize waits
    Updated the model to fix the route decision and optimize waiting times at stops. Adjusted parameters and constraints for a new set of candidate nodes.

--- 100nodes.jl
+++ 100nodes.jl
@@ -22,7 +22,7 @@ using Random
 Random.seed!(90)
 
 # ---------- instance ----------
-N       = 2221690                       # candidate customer nodes (was 5)
+N       = 2675440, 2675440                      # candidate customer nodes (was 5)
 nodes   = 1:N
 nodes_0 = 0:N
 T_end   = 30.0
