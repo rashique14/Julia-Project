8f8c0fde1885d4661847a61a4ee5ebd845c46190
rashique14 <r_itquan@live.concordia.ca>
2026-09-22 14:42:01 -0400

    Refactor model to use fixed route for optimization

--- 100nodes.jl
+++ 100nodes.jl
@@ -22,7 +22,7 @@ using Random
 Random.seed!(90)
 
 # ---------- instance ----------
-N       = 3002828, 3008579, 3015046-P, 3016790-S                      # candidate customer nodes (was 5)
+N       = 3002828, 3008579, 3015046-P, 3016790-S, 3716505-G                     # candidate customer nodes (was 5)
 nodes   = 1:N
 nodes_0 = 0:N
 T_end   = 30.0
